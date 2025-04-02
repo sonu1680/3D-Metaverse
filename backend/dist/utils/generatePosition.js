c0dd7e1fcb15481f1520668732e18b58a433e0c5
sonu1680 <sonupandit1680@gmail.com>
2025-04-02 22:33:07 +0530

    fix peerjs server issue

--- backend/dist/utils/generatePosition.js
+++ backend/dist/utils/generatePosition.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateRandomPosition = void 0;
 const generateRandomPosition = () => [
-    Math.random() * 260 - 80, // X-axis: -80 to 180
+    Math.random() * 260 - 80,
     -2,
-    Math.random() * 200 - 60, // Z-axis: -60 to 140
+    Math.random() * 200 - 60,
 ];
 exports.generateRandomPosition = generateRandomPosition;
